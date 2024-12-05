b90b0cffb823140cb95e6cd917b77a2efa38214a
antoniom <antonio.mavrin@prehnit.hr>
2024-12-05 13:46:53 +0100

    + added HTML elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
-function App() {
-  return <div className="App">APP</div>;
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.fetchWeather = this.fetchWeather.bind(this);
+
+    this.state = {
+      location: "",
+    };
+  }
+
+  fetchWeather() {
+    console.log("Loading data...");
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <h1>Classy Weather</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Search from location..."
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          />
+        </div>
+        <button onClick={this.fetchWeather}>Get weather</button>
+      </div>
+    );
+  }
 }
 
 export default App;
